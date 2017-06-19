1fdd47479bc665ed1d5977085b5c695311f7a68f
Dalthariel <david.hamaker@gmail.com>
2017-06-19 15:18:04 -0600

    Added some non-sequential functionality while backlogged nonworking model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # include DeviseWhitelist
+  # include SetSource
+  # include CurrentUserConcern
+  # include DefaultPageContent
+  # ^ those aren't built yet, this is for when they are
+
+  before_action :set_source
+	
+
+
 end
--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,11 +5,14 @@ class BlogsController < ApplicationController
   # GET /blogs.json
   def index
     @blogs = Blog.all # alternates can include .limit(1)
+    @page_title = "Portfolio Blog for David L. Hamaker"
   end
 
   # GET /blogs/1
   # GET /blogs/1.json
   def show
+    @page_title = @blog.title
+    @seo_keywords = @blog.body
   end
 
   # GET /blogs/new
